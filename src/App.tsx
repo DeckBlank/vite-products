c297310f5d638d070e0cbd3bf1ecba0ee3a17c5e
JOSE LUCIANO GUZMAN SANTIAGO <JOSE.GUZMAN@IBL.INETUM>
2024-08-29 11:42:29 -0500

    add: route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
-/* import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg' */
 import { lazy, Suspense } from 'react';
-//import './App.css'
-/* import { Header } from './ui/components/header/header';
-import { Sidebar } from './ui/components/sidebar/sidebar';
-import styles from './App.module.css' */
 import { Layout } from './ui/components/components';
 import { Route, Routes } from 'react-router-dom';
 
--- src/ui/components/sidebar/sidebar.tsx
+++ src/ui/components/sidebar/sidebar.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import styles from './sidebar.module.css';
-import { Link, Flex, Heading, Box, Text } from '@radix-ui/themes';
+import { Link, Flex, Heading, Box } from '@radix-ui/themes';
 
 
 
 export const Sidebar: React.FC<{ isVisible: boolean, className?: string }> = ({ className }) => {
   return (
     <Flex className={`${className} ${styles.sidebar}`} >
-      {/* <button className="close-button">✕</button> */}
       <Heading>Aplicación Base</Heading>
       <Box >
-        <Link href="/" weight="regular" className={styles.link}>
+        <Link href="/"  className={styles.link}>
           Sign in
         </Link>
       </Box>
